eee4d6683b04f79c54d241ae88d013645b854a5b
Yahya  Gok <ygok17@hotmail.com>
2020-02-06 16:28:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,7 +33,7 @@ array.any? {|word| word.length > 4 }
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(real_chesee)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
