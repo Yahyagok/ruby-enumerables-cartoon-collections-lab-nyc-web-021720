01fb7fce3d935a406c99aad3fc1e9809b1fe06a6
Yahya  Gok <ygok17@hotmail.com>
2020-02-06 16:23:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,7 @@ def summon_captain_planet(names)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
+array.any? {|word| word.legth > 4 }
   # Your code here
 end
 
